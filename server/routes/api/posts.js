3e7ae77090ca514bf1af03be15d1a0dfc1edc0e6
Hemalatha-kec <31584825+Hemalatha-kec@users.noreply.github.com>
2020-05-17 07:14:51 +0530

    first commit

---
+++ server/routes/api/posts.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+
+//Get Posts
+router.get("/", async (req, res) => {
+  const posts = await loadPostscollection();
+  res.send(await posts.find({}).toArray());
+});
+
+//Add Post
+router.post("/", async (req, res) => {
+  const posts = await loadPostscollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date(),
+  });
+  res.status(201).send();
+});
+
+//Delete Post
+router.delete("/:id", async (req, res) => {
+  const posts = await loadPostscollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
+});
+
+async function loadPostscollection() {
+  const client = await mongodb.MongoClient.connect(
+    "mongodb+srv://user:ajay123@cluster0-pkbwk.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+
+  return client.db("test").collection("posts");
+}
+
+module.exports = router;
